3a7ef4032b1b5106b1ce7440db99a57d100d7e2c
TruongDX8 <truonghusk17@gmail.com>
2021-03-20 05:06:36 +0700

    first commit

---
+++ src/a2_123456789/ProgStudentMan.java
@@ -0,0 +1,10 @@
+package a2_123456789;
+
+public class ProgStudentMan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
